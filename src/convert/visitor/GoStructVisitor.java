831e7256e34776dc5f442602c6b2155888f9b747
Jason Stillwell <dragonfax@gmail.com>
2021-12-18 02:08:24 -0800

    even simpler aggregate

--- src/convert/visitor/GoStructVisitor.java
+++ src/convert/visitor/GoStructVisitor.java
@@ -20,18 +20,10 @@ public class GoStructVisitor extends JavaParserBaseVisitor<Node> {
             return aggregate;
         }
     
-        if (! (aggregate instanceof FieldListNode )) {
-            return super.aggregateResult(aggregate, nextResult);
+        if ( aggregate instanceof FieldListNode && nextResult instanceof FieldListNode ) {
+            return ((FieldListNode)aggregate).append((FieldListNode)nextResult);
         }
 
-        var aggFieldList = (FieldListNode)aggregate;
-
-        if ( nextResult instanceof FieldListNode) {
-            return aggFieldList.append((FieldListNode)nextResult);
-        } else if ( nextResult instanceof FieldNode ) {
-            return aggFieldList.append(new FieldListNode((FieldNode)nextResult));
-        }
-    
         return super.aggregateResult(aggregate, nextResult);
     }
 
@@ -52,41 +44,18 @@ public class GoStructVisitor extends JavaParserBaseVisitor<Node> {
     @Override
     public Node visitFieldDeclaration(JavaParser.FieldDeclarationContext ctx) {
     
-        var type = ((FieldNode)visitTypeType(ctx.typeType())).type;
+        var type = ctx.typeType().getText();
 
         var varDecls = ctx.variableDeclarators().variableDeclarator();
 
         var fieldList = new ArrayList<FieldNode>(varDecls.size());
-    
         for ( var varDecl : varDecls )  {
-            var varDeclNode = visitVariableDeclarator(varDecl);
+            var name = varDecl.variableDeclaratorId().getText();
     
-            fieldList.add(new FieldNode( ((FieldNode)varDeclNode).name, type ));
+            fieldList.add(new FieldNode( name, type ));
         }
     
         return new FieldListNode(fieldList.toArray(new FieldNode[]{}));
-    }
-    
-    @Override
-    public Node visitVariableDeclaratorId(JavaParser.VariableDeclaratorIdContext ctx) {
-        // partial field node, just used to send part of the data up the line.
-        return new FieldNode(ctx.IDENTIFIER().getText(), null);
-    }
-    
-    @Override
-    public Node visitTypeType(JavaParser.TypeTypeContext ctx) {
-        // send partial field node, they get combined up the line.
-    
-        if ( ctx.primitiveType() != null ) {
-            return new FieldNode(null, ctx.primitiveType().getText());
-        }
-    
-        if ( ctx.classOrInterfaceType() != null ) {
-            var type = ctx.classOrInterfaceType().IDENTIFIER().get(0).getText();
-            return new FieldNode(null, type);
-        }
-    
-        throw new RuntimeException("unknown");
     }
     
 }
