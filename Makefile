d34c1eac8a186cc0f182f50037235443777b187b
Jason Stillwell <dragonfax@gmail.com>
2021-12-17 22:48:02 -0800

    java skeleton

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
+.PHONY: generate
 
+CLASSPATH="/usr/local/Cellar/antlr/4.9.3/antlr-4.9.3-complete.jar:."
 
 generate:
 	antlr -o parser JavaLexer.g4 JavaParser.g4
---
+++ src/convert/Main.java
@@ -0,0 +1,13 @@
+package convert;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class Main {
+    
+    public static  void main(String[] args) throws Exception {
+
+    }
+
+    
+}
